1d6116aba970d87109b523578635d1a745dca1ad
fatih <ffatihsann@gmail.com>
2022-04-09 00:42:36 +0300

    html parsing with request lib

---
+++ request.py
@@ -0,0 +1,31 @@
+import random
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = f"https://www.amazon.com.tr/s?k=iphone+13+pro+max+kılıfı+cüzdanlı&crid=13HRTL0XIZTFH&sprefix=iphone+13+pro+max+kılıfı+cüzdanlı%2Caps%2C103&ref=nb_sb_ss_ts-doa-p_1_33"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+}
+
+response = requests.get(URL, headers=headers)
+
+print(response.status_code)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+# print(soup.find_all(attrs={"data-asin" : "B09DGDB7DT"}))
+
+a = soup.find("div",class_="s-main-slot s-result-list s-search-results sg-row")
+
+scriptTags = a.findAll('div', attrs = {'data-asin' : True})
+
+for i in scriptTags:
+    print(i["data-asin"])
+
+
+
+
+
+
